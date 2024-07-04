c34f99ced434825393317c52471a288a1daf0f29
khakhiD <kakkiid@gmail.com>
2024-07-04 22:37:26 +0900

    remove: controller, service 제거

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
   imports: [],
-  /* 
-    controller:
-    - URL을 가져오고 함수를 실행함
-    - express의 라우터와 같은 존재
-  */
-  controllers: [AppController],
-  /* 
-    provider:
-    - 
-  */
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
